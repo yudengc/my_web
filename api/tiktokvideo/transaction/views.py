65ec3d849e2667c51c724c9ed295fc7a5580e0c0
lorcyu <luochuyuasd@gmail.com>
2020-11-23 18:43:38 +0800

    321

--- api/tiktokvideo/transaction/views.py
+++ api/tiktokvideo/transaction/views.py
@@ -39,11 +39,13 @@ class WeChatPayViewSet(APIView):
         else:
             return Response({"detail": 't_type错误'}, status=status.HTTP_400_BAD_REQUEST)
 
+        logger.info(money)
+        logger.info('1231231241241413123')
         order = OrderInfo.create_order(request.user, money, t_type, p_id)
         # 获取客户端ip
         client_ip = get_ip(request)
         attach = str(request.user.uid) + '_' + str(p_id)  # 自定义参数，回调要用
-        data = WeChatPay().pay(float(money), client_ip, order.out_trade_no, request.user.openid, attach)
+        data = WeChatPay().pay(money, client_ip, order.out_trade_no, request.user.openid, attach)
         # print(data)
         if data:
             return Response(data, status=status.HTTP_200_OK)
