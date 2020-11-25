4ae9d33603ad8fe0c13b2fa7ccbd5b6b709ff71b
yudengc <yudengc@outlook.com>
2020-11-25 11:51:47 +0800

    update

--- api/tiktokvideo/demand/urls.py
+++ api/tiktokvideo/demand/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from demand.views import VideoNeededViewSet
 
 app_name = "demand"
 
 router = DefaultRouter()
-router.register(r'video', )
+router.register(r'video', VideoNeededViewSet, basename='video')
 # router.register(r'customer-service', CustomerServiceViewSet, basename='login')
 
 
