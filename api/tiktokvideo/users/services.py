18ce39f77cf5fb298d66ac7cf77f65a59412a5b5
yudengc <yudengc@outlook.com>
2020-12-14 11:11:01 +0800

    update

--- api/tiktokvideo/users/services.py
+++ api/tiktokvideo/users/services.py
@@ -9,6 +9,11 @@ import json
 
 import requests
 from Crypto.Cipher import AES
+from django.conf import settings
+from django_redis import get_redis_connection
+from redis import StrictRedis
+
+conn = get_redis_connection('default')  # type: StrictRedis
 
 
 class WeChatApi:
@@ -55,7 +60,7 @@ class WXBizDataCrypt:
             return False
 
     def _unpad(self, s):
-        return s[:-ord(s[len(s)-1:])]
+        return s[:-ord(s[len(s) - 1:])]
 
 
 class InviteCls:
@@ -122,3 +127,83 @@ class InviteCls:
 
         res = ((res + real_idx[0]) - cls.slat) // cls.prime1
         return res
+
+
+class WeChatOfficial:
+    """
+    公众号
+    """
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance:
+            return cls._instance
+        this_instance = super(WeChatOfficial, cls).__new__(cls, *args, **kwargs)
+        cls._instance = this_instance
+        return this_instance
+
+    def __init__(self) -> None:
+        self.expired_time = 3600
+
+    def get_access_token(self):
+        arg = {
+            'grant_type': 'client_credential',
+            'appid': settings.WECHAT_OFFICIAL_APPID,
+            'secret': settings.WECHAT_OFFICIAL_APPSECRET
+        }
+        base_url = "https://api.weixin.qq.com/cgi-bin/token"
+        access_token = conn.get('wx_access_token')
+        if access_token:
+            return access_token
+
+        req_obj = requests.get(base_url, params=arg)
+        access_token = json.loads(req_obj.content).get('access_token', None)
+        if not access_token:
+            raise ValueError(f"token获取失败:{json.loads(req_obj.content)}")
+        else:
+            conn.set('wx_access_token', access_token, self.expired_time)
+        return access_token
+
+    def get_ticket(self, uid):
+        url = f"https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token={self.get_access_token()}"
+        header = {
+            "Content-Type": "application/json"
+        }
+        arg = {
+            "expire_seconds": 180,  # 3分钟超时
+            "action_name": "QR_SCENE",
+            "action_info": {
+                "scene": {
+                    "scene_id": uid
+                }
+            }
+        }
+        req_content = json.loads(requests.post(url, json=arg, headers=header).content)
+        ticket = req_content.get('ticket', None)
+        return ticket, req_content
+
+    def get_qr_url(self, uid=123):
+        ticket, req_content = self.get_ticket(uid)
+        if not ticket:
+            if str(req_content.get('errcode')) == '40001':
+                # 删掉token缓存再执行一次
+                conn.delete('wx_access_token')
+                ticket, req_content = self.get_ticket(uid)
+                if not ticket:
+                    raise ValueError(f'ticket获取失败:{req_content}')
+
+        qr_url = f"https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket={ticket}"
+        return qr_url
+
+    def get_user_info(self, open_id):
+        url = "https://api.weixin.qq.com/cgi-bin/user/info"
+        arg = {
+            'access_token': self.get_access_token(),
+            'openid': open_id,
+            'lang': "zh_CN"
+        }
+        req_content = json.loads(requests.get(url, arg).content)
+        # union_id = req_content.get('unionid', None)
+        # if not union_id:
+        #     raise ValueError(f"unionid获取失败:{req_content}")
+        return req_content
