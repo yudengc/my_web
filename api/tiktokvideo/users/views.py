73890f70dc8be1785cca84f14e60e98aaa53e34f
yudengc <yudengc@outlook.com>
2020-12-18 17:43:53 +0800

    update

--- api/tiktokvideo/users/views.py
+++ api/tiktokvideo/users/views.py
@@ -884,7 +884,7 @@ class PublicWeChat(APIView):
                     success = OfficialAccountMsg.template_send(obj, **form)
             except Exception as e:
                 logger.info(traceback.format_exc())
-                return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response({"detail": '服务器开小差了!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             else:
                 _msg = "已成功发送"
                 if un_match_list:
